8c3b2b43a4197ef16549df0d6eefed6b898bf854
amahdy@turndigital.net <aya.mahdy512000@gmail.com>
2024-01-08 22:22:16 +0200

    fix: table

--- components/table-sec/table.vue
+++ components/table-sec/table.vue
@@ -1,42 +1,44 @@
 <template>
-  <table class="border-collapse border border-slate-400 table-auto w-full mt-5">
-    <thead>
-      <tr>
-        <th
-          v-for="(cell, index) in item"
-          :key="index"
-          class="border border-slate-300 px-4 py-2"
-        >
-          {{ cell.parent }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td
-          class="border border-slate-300 px-4 py-2"
-          v-for="(cell, index) in item"
-          :key="index"
-        >
-          {{ cell.name ? cell.name : "-" }}
-          <span v-if="cell.id == ''">/ {{ cell.userInput }}</span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table">
+    <table
+      class="border-collapse border border-slate-400 table-auto w-full mt-5"
+    >
+      <thead>
+        <tr>
+          <th
+            v-for="(cell, index) in item"
+            :key="index"
+            class="border border-slate-300 px-4 py-2"
+          >
+            {{ cell.parent }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td
+            class="border border-slate-300 px-4 py-2"
+            v-for="(cell, index) in item"
+            :key="index"
+          >
+            {{ cell.name ? cell.name : "-" }}
+            <span v-if="cell.id == ''">/ {{ cell.userInput }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
 export default {
   props: ["item"],
   data() {
-    return {
-    };
+    return {};
   },
   mounted() {
-
     for (const key in this.item) {
-      if(typeof this.item[key] == 'string'){
-        delete this.item[key]
+      if (typeof this.item[key] == "string") {
+        delete this.item[key];
       }
     }
   },
